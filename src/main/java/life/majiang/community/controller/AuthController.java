fe752f7a9204fb79ff5df2db548f83c2a1b3f1af
tjbaka <tjbaka@163.com>
2020-03-29 10:45:00 +0800

    session and cookie

--- src/main/java/life/majiang/community/controller/AuthController.java
+++ src/main/java/life/majiang/community/controller/AuthController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -31,7 +33,8 @@ public class AuthController {
 
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code")String code, @RequestParam(name="state")String state, HttpServletRequest request){
+    public String callback(@RequestParam(name="code")String code, @RequestParam(name="state")String state,
+                           HttpServletRequest request , HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -42,14 +45,15 @@ public class AuthController {
         GithubUser githubUser = gitHubProvider.getUser(accessToken);
         if(githubUser!=null){
             User user=new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(System.currentTimeMillis());
             userMapper.insert(user);
-            //写入成功，添加cookie和session
-            request.getSession().setAttribute("githubUser",githubUser);
+            //写入成功，添加cookie
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
             //登入失败
---
+++ src/main/java/life/majiang/community/controller/IndexContrller.java
@@ -0,0 +1,39 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.Model.User;
+import life.majiang.community.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexContrller {
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public String hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
+        return "index";
+    }
+
+
+
+}
